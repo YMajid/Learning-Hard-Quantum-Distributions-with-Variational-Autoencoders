1c5fec1056d758ec860178caa3281c4894072508
Alexander Nilo Fogal <anfogal@uwaterloo.ca>
2021-03-28 13:29:10 -0400

    Made create dataset function

--- src/utils/data_reader.py
+++ src/utils/data_reader.py
@@ -1,5 +1,5 @@
 import csv
-
+import gen_easy, gen_random, gen_hard
 
 def data_reader(csv_path):
     """
@@ -21,3 +21,28 @@ def data_reader(csv_path):
             labels.append(row_array[-1])
 
     return features, labels
+
+def create_dataset(n_qubits):
+    times = np.arange(0, 5.01, 1.0)
+    L=8
+
+    easy = EasyStateGenerator()
+    easy_d = easy.get_time_evolve_state(L, times)
+    np.savez("easy_dset", easy_dset=easy_d)
+    del easy_d
+
+    rand = RandomStateGenerator()
+    rand_d = rand.gen_unitary(n_qubits)
+    np.savez("rand_dset", rand_dset=rand_d )
+    del rand_d
+
+    hard = HardStateGenerator(n_qubits, L)
+    hard_d = get_hard_distribution(mode="full")
+    np.savez("hard_dset", hard_dset=hard_d)
+    del hard_d
+
+
+
+
+
+
